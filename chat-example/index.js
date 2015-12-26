37e9712a656a1d65bfa22584d339374f6b9d90b4
Hila b <ba.hilali@gmail.com>
2015-12-26 14:40:53 +0200

    basic_chat

---
+++ chat-example/index.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Hila on 23/12/2015.
+ */
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+
+    console.log('a user connected');
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
