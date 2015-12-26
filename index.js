3a97a7ce790f5a604c738d7c2f492f4a7a4dc4de
Hila b <ba.hilali@gmail.com>
2015-12-26 15:01:30 +0200

    change index location

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + 'chat-example/index.html');
 });
 
 io.on('connection', function(socket){
