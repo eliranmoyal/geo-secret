c9c282f702e42d0e12c39ff315dea57afbaf5181
eliran moyal <eliran.moyal1@gmail.com>
2016-01-17 23:35:37 +0200

    custom post on form.. sending all data except public and private key

--- secret-communicator/js/controller.js
+++ secret-communicator/js/controller.js
@@ -1,8 +1,12 @@
 
+var facebookId;
+var facebookToken;
 function afterFacebookLogin(id,tokenId){
 	divToAppear = "#ring-container";
 	console.log("on controller afterFacebookLogin");
 	console.log ("id:" + id);
+	facebookId  = id;
+	facebookToken = tokenId;
 	console.log("tokenId:" + tokenId);
     $('#login-container').addClass("animated").addClass("fadeOutUpBig");
     setTimeout(function() {
@@ -11,3 +15,30 @@ function afterFacebookLogin(id,tokenId){
     }, 200);
     
 }
+
+function getKey () {
+	return {
+		"public":"hey",
+		"encrypted_private":"bye"
+	};
+}
+
+$(document).ready(function(){
+     $('#registerForm').submit(function (e) {
+         e.preventDefault();
+         console.log("register...");
+         data = {};
+         data["token"] = facebookToken;
+         data["social_id"] = facebookId;
+         data["social_type"] = "facebook";
+         key = getKey();
+         data["public_key"] = key.public;
+         data["encrypted_private_key"] = key.encrypted_private;
+         data["ring"] = $("#ring").val();
+         
+        $.post( "/register", data)
+		  .done(function( result ) {
+		    console.log( "Data Loaded: " + result );
+		  });
+     });
+});
